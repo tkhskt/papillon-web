5b6d62804bf7bce162c87988dc14a5719f140e9d
高橋啓太 <tkhs@takahashikeitanoiMac.local>
2021-10-24 03:54:49 +0900

    fix: circle

--- components/atoms/GradientCircle.vue
+++ components/atoms/GradientCircle.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="circle-container"></div>
+  <div
+    class="circle-container"
+    @mouseenter="hoverCircle = true"
+    @mouseleave="hoverCircle = false"
+  >
+    <p class="announce" :class="{ visible: hoverCircle }">
+      The limited edition releases later…
+    </p>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      hoverCircle: false,
+    }
+  },
+}
+</script>
 
 <style scoped lang="scss">
 .circle-container {
+  position: relative;
   width: 28.6vw;
   height: 28.6vw;
   border-radius: 50%;
@@ -13,6 +30,25 @@
   @include mq() {
     width: 100vmin;
     height: 100vmin;
+  }
+  .announce {
+    position: absolute;
+    font-size: 18px;
+    top: calc(50% - 30px);
+    left: 0;
+    right: 0;
+    margin: auto;
+    text-align: center;
+    color: white;
+    @include font-hightower();
+    opacity: 0;
+    transition: opacity 0.5s ease;
+    &.visible {
+      opacity: 0.4;
+    }
+    @include mq() {
+      display: none;
+    }
   }
 }
 </style>
